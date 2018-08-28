a07d9d626872e027c238fe55b62d9c35585a3e3c
Cecil Wöbker <me@cwoebker.com>
2018-08-28 21:00:15 +0200

    Bump version to 0.4.1

--- pen/__init__.py
+++ pen/__init__.py
@@ -5,6 +5,6 @@ pen: terminal notes
 
 __title__ = 'pen'
 __author__ = 'cwoebker'
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 __license__ = 'BSD'
-__copyright__ = '(c) 2013-2018 Cecil Woebker'
+__copyright__ = '© 2013-2018 Cecil Wöbker'
