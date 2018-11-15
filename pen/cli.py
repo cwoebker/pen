ee4ce8bec53a37991a68a935b7c474e8bbb69876
Cecil Wöbker <me@cwoebker.com>
2018-11-15 22:02:18 +0100

    Update command author message

--- pen/cli.py
+++ pen/cli.py
@@ -9,7 +9,7 @@ from pen import __version__
 from pen.core import cmd_touch_note, cmd_list
 from pen.storage import penStore
 
-app = Paxo('pen', 'a Cecil Woebker project.', '<command> | <note>',
+app = Paxo('pen', 'by Cecil Wöbker', '<command> | <note>',
            __version__, default_action=cmd_list, dynamic_action=cmd_touch_note, store=penStore)
 
 
